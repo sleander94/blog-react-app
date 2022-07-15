b9162a2b69de9c0cdb6ca91be7ae4bd05bcf948c
Stephen Leander <stephen.leander1@gmail.com>
2022-07-15 17:53:30 -0600

    Create initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Posts from './components/Posts';
+import Post from './components/Post';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Blog</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/posts" />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path={`/posts/:id`} element={<Post />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 };
