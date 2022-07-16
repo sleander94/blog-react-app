4499ff280019cf89bd813338c63e6aceae693e53
Stephen Leander <stephen.leander1@gmail.com>
2022-07-16 15:33:58 -0600

    Import materialui Appbar & modify links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,29 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 const Navbar = () => {
-  return <nav id="navbar">Nav</nav>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Button color="inherit" href="/posts">
+              Blog
+            </Button>
+          </Typography>
+          <Button color="inherit" href="/signup">
+            Sign Up
+          </Button>
+          <Button color="inherit" href="/login">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Navbar;
