5f6c5ca62c08e0e73633c8965e06578fefdac536
Stephen Leander <stephen.leander1@gmail.com>
2022-07-15 20:03:03 -0600

    Fetch posts data from API and display links to posts

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { post } from '../types.d';
+
 const Posts = () => {
-  return <section id="posts">Posts</section>;
+  const [posts, setPosts] = useState<post[]>([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await fetch('/posts');
+        const data = await response.json();
+        setPosts(data);
+        console.log(posts);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getPosts();
+  }, []);
+  return (
+    <section id="posts">
+      <h1>Posts</h1>
+      {posts.map((post) => {
+        return (
+          <Link to={`/posts/${post._id}`} key={posts.indexOf(post)}>
+            {' '}
+            <p>{post.title}</p>
+            <p>
+              {post.author} | {post.timestamp}
+            </p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Posts;
