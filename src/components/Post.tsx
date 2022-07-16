0601f6648d45f3013e673d4a7a0508928846b0ce
Stephen Leander <stephen.leander1@gmail.com>
2022-07-15 20:03:48 -0600

    Fetch post data with comments from API and render on page

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,56 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { post, comment } from '../types.d';
+
 const Post = () => {
-  return <section id="post">Post</section>;
+  const [post, setPost] = useState<post>({
+    author: '',
+    authorId: '',
+    isPublic: false,
+    text: '',
+    timestamp: '',
+    title: '',
+    __v: 0,
+    _id: '',
+  });
+  const [comments, setComments] = useState<comment[]>([]);
+  const { id } = useParams();
+  useEffect(() => {
+    const getPostAndComments = async () => {
+      try {
+        const response = await fetch(`/posts/${id}`);
+        const data = await response.json();
+        setPost(data.post);
+        setComments(data.comments);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getPostAndComments();
+  }, []);
+
+  return (
+    <section id="post">
+      <div className="post">
+        <h1>{post.title}</h1>
+        <p>
+          {post.author} | {post.timestamp}
+        </p>
+        <p>{post.text}</p>
+      </div>
+      <div className="comments">
+        {comments.map((comment) => {
+          return (
+            <div className="comment" key={comments.indexOf(comment)}>
+              <p>{comment.author}</p>
+              <p>{comment.text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Post;
