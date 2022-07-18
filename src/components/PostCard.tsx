e188749631b7771ef214e66de3e685bdeb304d2b
Stephen Leander <stephen.leander1@gmail.com>
2022-07-18 12:52:18 -0600

    Initial commit

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { postCardProps } from '../types.d';
+
+const PostCard = ({ title, date, id }: postCardProps) => {
+  return (
+    <Card sx={{ maxWidth: 345, bgcolor: '#e5e5e5' }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {date}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" href={`/posts/${id}`}>
+          See solution
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default PostCard;
