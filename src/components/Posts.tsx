daaf600e611684b334b3881eaf7422348a328ec5
Stephen Leander <stephen.leander1@gmail.com>
2022-08-09 19:47:08 -0600

    Add loading bar

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from 'react';
 import { post } from '../types.d';
 import PostCard from './PostCard';
 import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
+import { LinearProgress, Typography } from '@mui/material';
 import { decode } from 'html-entities';
 
 const Posts = () => {
   const [posts, setPosts] = useState<post[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     const getPosts = async () => {
       try {
@@ -15,6 +16,7 @@ const Posts = () => {
         );
         const data = await response.json();
         setPosts(data);
+        setIsLoading(false);
       } catch (err) {
         console.error(err);
       }
@@ -26,24 +28,33 @@ const Posts = () => {
       <Typography component="h1" variant="h3" align="center" sx={{ mb: 4 }}>
         Solved Problems
       </Typography>
-      <Grid container spacing={4} justifyContent="center">
-        {posts.map((post) => {
-          return (
-            <Grid
-              item
-              xs={8}
-              md={4}
-              key={posts.indexOf(post)}
-              sx={{ display: 'flex', justifyContent: 'center' }}
-            >
-              <PostCard
-                title={decode(post.title)}
-                date={decode(post.timestamp)}
-                id={decode(post._id)}
-              />
-            </Grid>
-          );
-        })}
+      <Grid
+        container
+        spacing={4}
+        justifyContent="center"
+        sx={{ minHeight: '50vh' }}
+      >
+        {isLoading && (
+          <LinearProgress sx={{ width: '50%', alignSelf: 'center' }} />
+        )}
+        {!isLoading &&
+          posts.map((post) => {
+            return (
+              <Grid
+                item
+                xs={8}
+                md={4}
+                key={posts.indexOf(post)}
+                sx={{ display: 'flex', justifyContent: 'center' }}
+              >
+                <PostCard
+                  title={decode(post.title)}
+                  date={decode(post.timestamp)}
+                  id={decode(post._id)}
+                />
+              </Grid>
+            );
+          })}
       </Grid>
     </section>
   );
